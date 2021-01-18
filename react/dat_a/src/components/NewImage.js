14d4149d2ae9e734782facfcba38b34d4a4932e1
amsks <adityak735@gmail.com>
2021-01-18 22:48:22 +0100

    Multiple Images Upload

--- react/dat_a/src/components/NewImage.js
+++ react/dat_a/src/components/NewImage.js
@@ -1,6 +1,7 @@
 import React, {useState} from 'react'
 import firebase from 'firebase'
 import {app} from '../base'
+import Dropzone from "./DropZone"
 
 // Get the db reference and the storage reference 
 const db = app.firestore()
@@ -11,31 +12,47 @@ const storage = app.storage();
 export const NewImage = ({currentTask}) => {
   
     // Initialize the File variable and the setFile function  
-  const [file, setFile] = useState(null)
+  const [file, setFile] = useState([])
 
   // On changing the file, set the target based on event 
-  const onFileChange = (e) => {
-    setFile(e.target.files[0])
+  const onFileChange = e => {
+    
+    for (let i = 0; i < e.target.files.length; i++) {
+      const newFile = e.target.files[i];
+      newFile["id"] = Math.random();
+   // add an "id" property to each File object
+      setFile(prevState => [...prevState, newFile]);
+    }
+    // setFile(e.target.files[0])
   }
 
   // Function to  handle the upload of image
-  const onUpload = async () => {
-    const storageRef = storage.ref()
-    const fileRef = storageRef.child(file.name)
-    await fileRef.put(file)
+  const onUpload = (e) => {
     
-    db.collection("tasks").doc(currentTask).update({
+    file.forEach(async element => {
+      const storageRef = storage.ref()
+      const fileRef = storageRef.child(element.name)
+      await fileRef.put(element)
+
+      db.collection("tasks").doc(currentTask).update({
       
       images: firebase.firestore.FieldValue.arrayUnion({
-                                                          name: file.name,
+                                                          name: element.name,
                                                           url: await fileRef.getDownloadURL(), 
                                                           annotation: " "
                                                       })
-    })
+      })
+
+
+    });  
   }
 
   return <>
-    <input type="file" onChange={onFileChange}/>
+
+    {/* <Dropzone/> */}
+    <input type="file" multiple onChange={onFileChange}/>
+
+    
     <button onClick={onUpload}>Upload Image</button>
   </>
 }
